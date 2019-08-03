7dc38232d22978ea524bb4665266c388e802c5f1
Ironbatpug <molnarcsaba92@gmail.com>
2019-08-03 10:29:04 +0200

    gitignore added

---
+++ Threads/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Threads
+//
+//  Created by Molnár Csaba on 2019. 08. 03..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Threads/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Threads
+//
+//  Created by Molnár Csaba on 2019. 08. 03..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
