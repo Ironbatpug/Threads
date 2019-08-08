d8d9aa25497a56885c7744d0e509c2a8d698d106
Ironbatpug <molnarcsaba92@gmail.com>
2019-08-08 20:51:02 +0200

    lesson-12 tableview run history

---
+++ Threads/View/RunLogCell.swift
@@ -0,0 +1,24 @@
+//
+//  RunLogCell.swift
+//  Threads
+//
+//  Created by Molnár Csaba on 2019. 08. 08..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class RunLogCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
